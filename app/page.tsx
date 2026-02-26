4d1faa7db8f9922c099111aba38d53f102c48c3f
arippicnic <arippicnic@gmail.com>
2026-02-26 18:53:13 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { PersonScene } from "../components/PersonScene";
+
+export default function Home() {
+  return (
+    <main className="flex h-screen w-screen bg-black text-white">
+      <PersonScene />
+    </main>
+  );
+}
